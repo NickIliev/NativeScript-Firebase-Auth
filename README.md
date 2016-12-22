45c99b6246895af734c6264685825682c897753a
NickIliev <Nikolay.Iliev@telerik.com>
2016-12-22 13:58:27 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # NativeScript-Firebase-FacebookAuth
+
+
+Steps to implement facebook auth via firebase in NativeScrip app.
+
+## Facebook console part
+1. log in https://developers.facebook.com/apps/
+2. go to Settings >> Basic and add Android platform
+3. check **AppId** and **AppSecret** (will be needed in firebase console later) 
+    AppId:      242365049527563
+    AppSecret:  babeadab2f846955387c2836e2b37530
+
+4. for **Google Play Package Name** add applicationId from your package.json (e.g. in this case `org.nativescript.firebaseAuth`)
+5. for **Class Name** add `com.tns.NativeScriptActivity`
+6. the final step is to add your **Key Hashes** (if you do not know them now, do not worry - we will add them at our final step!)
+
+## Firebase console part
+1. log in https://console.firebase.google.com
+2. go to Authentication
+3. enable Facebook Authentication
+4. add AppID and AppSecret from the Facebook console
+5. save all
+
+## Project Steps
+1. if you have previously included Firebase plugin delete the platforms folder (do not save your google-services.json as you will need newly generated one)
+2. if you do not have the plugin install it with `tns plugin add nativescript-plugin-firebase`
+3. open your `firebase.nativescript.json` and make sure that `"facebook_auth": true`
+4. `tns platform add android`
+5. generate your google-services.json from https://console.firebase.google.com  (make new one with the facebook enabled!!!)
+6. paste google-services.json in `platforms/Android/`
+7. go to `app/App_Resources/Android/AndroidManifest.xml` and add `<meta-data android:name="com.facebook.sdk.ApplicationId" android:value="@string/facebook_app_id"/>` 
+    Add it as a child of application on the activity level.
+
+8. go to `app/App_Resources/Android/values` folder and create file called `facebooklogin.xml` with content
+    ```
+        <?xml version='1.0' encoding='utf-8'?>
+        <resources>
+            <string name="facebook_app_id">1112223334445556</string>
+        </resources>
+    ```
+    The value for `facebook_app_id` is your Facebook AppId
+
+9. initialize firebase if not already done: 
+    ```
+        firebase.init({
+            onAuthStateChanged: function (data) { // optional but useful to immediately re-logon the user when he re-visits your app
+                console.log(data.loggedIn ? "Logged in to firebase" : "Logged out from firebase");
+                if (data.loggedIn) {
+                    console.log("user's email address: " + (data.user.email ? data.user.email : "N/A"));
+                }
+            },
+
+        }).then(instance => {
+            console.log("firebase.init done");
+        }).catch(err => {
+            console.log("Firebase init error: " + err);
+        });
+    ```
+
+10. finally implement the login for Facebook
+    ```
+    firebase.login({
+        type: firebase.LoginType.FACEBOOK,
+        scope: ['public_profile', 'email'] // optional: defaults to ['public_profile', 'email']
+    }).then(
+        function (result) {
+            console.log(JSON.stringify(result));
+
+            frame.topmost().navigate("./views/drawer-page");
+        },
+        function (errorMessage) {
+            console.log(errorMessage);
+        }
+    );
+    ```
+
+11. Remember those **Key Hashes** we need to set in Facebook console!? Well after your first attempt to login in,
+    open `adb logcat` and look for something like Key hash <......> does not match any stored key hashes.
+    Copy the hash key and paste it to the faceboook developer console. Voila!
---
+++ firebaseAuth/node_modules/tns-core-modules/ui/styling/background.android.js
@@ -0,0 +1,115 @@
+var utils = require("utils/utils");
+var common = require("./background-common");
+var types = require("utils/types");
+var cssValue = require("css-value");
+var button;
+var style;
+function ensureLazyRequires() {
+    if (!button) {
+        button = require("ui/button");
+    }
+    if (!style) {
+        style = require("./style");
+    }
+}
+global.moduleMerge(common, exports);
+var ad;
+(function (ad) {
+    var SDK;
+    function getSDK() {
+        if (!SDK) {
+            SDK = android.os.Build.VERSION.SDK_INT;
+        }
+        return SDK;
+    }
+    var _defaultBackgrounds = new Map();
+    function onBackgroundOrBorderPropertyChanged(v) {
+        var nativeView = v._nativeView;
+        if (!nativeView) {
+            return;
+        }
+        ensureLazyRequires();
+        var background = v.style._getValue(style.backgroundInternalProperty);
+        var backgroundDrawable = nativeView.getBackground();
+        var density = utils.layout.getDisplayDensity();
+        var cache = v._nativeView;
+        if (v instanceof button.Button
+            && !types.isNullOrUndefined(backgroundDrawable)
+            && types.isFunction(backgroundDrawable.setColorFilter)
+            && !background.hasBorderWidth()
+            && !background.hasBorderRadius()
+            && !background.clipPath
+            && types.isNullOrUndefined(background.image)
+            && !types.isNullOrUndefined(background.color)) {
+            var backgroundColor = backgroundDrawable.backgroundColor = background.color.android;
+            backgroundDrawable.setColorFilter(backgroundColor, android.graphics.PorterDuff.Mode.SRC_IN);
+            backgroundDrawable.backgroundColor = backgroundColor;
+        }
+        else if (!background.isEmpty()) {
+            if (!(backgroundDrawable instanceof org.nativescript.widgets.BorderDrawable)) {
+                var viewClass = types.getClass(v);
+                if (!(v instanceof button.Button) && !_defaultBackgrounds.has(viewClass)) {
+                    _defaultBackgrounds.set(viewClass, nativeView.getBackground());
+                }
+                backgroundDrawable = new org.nativescript.widgets.BorderDrawable(density);
+                refreshBorderDrawable(v, backgroundDrawable);
+                nativeView.setBackground(backgroundDrawable);
+            }
+            else {
+                refreshBorderDrawable(v, backgroundDrawable);
+            }
+            if ((background.hasBorderWidth() || background.hasBorderRadius() || background.clipPath) && getSDK() < 18) {
+                cache.layerType = cache.getLayerType();
+                cache.setLayerType(android.view.View.LAYER_TYPE_SOFTWARE, null);
+            }
+        }
+        else {
+            if (v instanceof button.Button) {
+                var nativeButton = new android.widget.Button(nativeView.getContext());
+                nativeView.setBackground(nativeButton.getBackground());
+            }
+            else {
+                var viewClass = types.getClass(v);
+                if (_defaultBackgrounds.has(viewClass)) {
+                    nativeView.setBackground(_defaultBackgrounds.get(viewClass));
+                }
+            }
+            if (cache.layerType !== undefined) {
+                cache.setLayerType(cache.layerType, null);
+                cache.layerType = undefined;
+            }
+        }
+        var leftPadding = Math.round(((background.borderLeftWidth || 0) + (v.style.paddingLeft || 0)) * density);
+        var topPadding = Math.round(((background.borderTopWidth || 0) + (v.style.paddingTop || 0)) * density);
+        var rightPadding = Math.round(((background.borderRightWidth || 0) + (v.style.paddingRight || 0)) * density);
+        var bottomPadding = Math.round(((background.borderBottomWidth || 0) + (v.style.paddingBottom || 0)) * density);
+        nativeView.setPadding(leftPadding, topPadding, rightPadding, bottomPadding);
+    }
+    ad.onBackgroundOrBorderPropertyChanged = onBackgroundOrBorderPropertyChanged;
+})(ad = exports.ad || (exports.ad = {}));
+function refreshBorderDrawable(view, borderDrawable) {
+    var background = view.style._getValue(style.backgroundInternalProperty);
+    if (background) {
+        var backgroundPositionParsedCSSValues = null;
+        var backgroundSizeParsedCSSValues = null;
+        if (background.position) {
+            backgroundPositionParsedCSSValues = createNativeCSSValueArray(background.position);
+        }
+        if (background.size) {
+            backgroundSizeParsedCSSValues = createNativeCSSValueArray(background.size);
+        }
+        borderDrawable.refresh((background.borderTopColor && background.borderTopColor.android) ? background.borderTopColor.android : 0, (background.borderRightColor && background.borderRightColor.android) ? background.borderRightColor.android : 0, (background.borderBottomColor && background.borderBottomColor.android) ? background.borderBottomColor.android : 0, (background.borderLeftColor && background.borderLeftColor.android) ? background.borderLeftColor.android : 0, background.borderTopWidth, background.borderRightWidth, background.borderBottomWidth, background.borderLeftWidth, background.borderTopLeftRadius, background.borderTopRightRadius, background.borderBottomRightRadius, background.borderBottomLeftRadius, background.clipPath, (background.color && background.color.android) ? background.color.android : 0, (background.image && background.image.android) ? background.image.android : null, background.repeat, background.position, backgroundPositionParsedCSSValues, background.size, backgroundSizeParsedCSSValues);
+    }
+}
+function createNativeCSSValueArray(css) {
+    if (!css) {
+        return null;
+    }
+    var cssValues = cssValue(css);
+    var nativeArray = Array.create(org.nativescript.widgets.CSSValue, cssValues.length);
+    for (var i = 0, length = cssValues.length; i < length; i++) {
+        nativeArray[i] = new org.nativescript.widgets.CSSValue(cssValues[i].type, cssValues[i].string, cssValues[i].unit, cssValues[i].value);
+    }
+    return nativeArray;
+}
+//# sourceMappingURL=background.android.js.map
